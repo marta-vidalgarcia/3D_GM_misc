09e206ddc063f9ee384eeb5ed71a06e37139b961
Marta <marta.vidalga@gmail.com>
2020-02-26 14:57:25 -0700

    Bash script to create a tps file from multiple .tag landmark files from MINC

---
+++ tag2tps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create a *.tps file from multiple *.tag files (landmark files from MINC)
+
+for i in *.tag  
+do
+   sed -i -e '/MNI Tag Point File/,+3d' $i
+   spec_name=$i
+   ID="ID=${spec_name}"
+   sed -i -e '/Volumes = 1;/,+3d' $i
+   sed -i '1s/^/LM3=249/' $i
+   echo $ID >> $i
+   echo "SCALE=1" >> $i
+   echo "" >> $i
+   sed -i 's/ 0.3 1 1 "curve_semilandmark"//g' $i
+   sed -i 's/ 0.5 1 1 "Landmark"//g' $i
+   sed -i 's/ 0.2 1 1 "curve_semilandmark"//g' $i
+   sed -i 's/;//g' $i
+   sed -i 's/_endocast.tag//g' $i
+   sed -i 's/Points =//g' $i
+   sed -i 's/Points =//g' $i
+   sed "s/^[ \t]*//" -i $i
+done
+
+cat *.tag >> DATASET_endocast_landmarks.tps
